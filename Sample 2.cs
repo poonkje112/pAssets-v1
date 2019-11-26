e3adb0c9a91aabe8936a94f720bf74afea5a7aa3
poonkje112 <aaron.knoop@live.nl>
2019-11-26 12:23:12 +0100

    Create Sample 2.cs

---
+++ Sample 2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.poonkje.Sample
+{
+    public class Sample : MonoBehaviour
+    {
+        public int test = 5;
+        public GameObject cube;
+    }
+}
